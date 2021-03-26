c354efe84c24dd57fbabf9d97263ff0f6b46f0c2
Adrian Kolano <kolanoadrian@hotmail.com>
2021-03-26 08:59:22 +0100

    the first page structure was prepared and add baceground

---
+++ script.js
@@ -0,0 +1,10 @@
+const sayHello = () => {
+    console.log("Hello Frond__EndDevelopers")
+}
+
+sayHello();
+
+window.addEventListener("load", function () {
+    console.log("The page has loaded");
+});
+
