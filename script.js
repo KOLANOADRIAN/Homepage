ef4e1a3f7797ce267f9d9cde1de0d7752d3c22f5
Adrian Kolano <kolanoadrian@hotmail.com>
2021-03-26 14:10:38 +0100

    add background url foto and style css .....all in progres...

--- script.js
+++ script.js
@@ -13,11 +13,12 @@ window.addEventListener("load", function () {
 
 const container__start = document.querySelector(".container__start");
 const container_startButton  = document.querySelector(".container_startButton");
+const containerOff = document.querySelector(".containerOff")
 
 container_startButton.addEventListener("click", () => {
-    container__start.classList.toggle("container__startOff")
-    container_startButton.classList.toggle("container_startButtonOff")
-
+    container__start.classList.toggle("container__startOff");
+    container_startButton.classList.toggle("container_startButtonOff");
+    containerOff.classList.toggle("container");
 });
 
 
